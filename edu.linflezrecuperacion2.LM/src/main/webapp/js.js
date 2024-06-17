8bf348adc41ae92834d171d90b7a64c984f6f7bf
Linfantee <145344308+Linfantee@users.noreply.github.com>
2024-06-17 12:57:45 +0200

    no message

---
+++ edu.linflezrecuperacion2.LM/src/main/webapp/js.js
@@ -0,0 +1,41 @@
+/**
+ * 
+ */
+let palabra;
+
+ 
+ function introducirPalabra(){
+	 
+	 do{
+		 
+		 palabra = prompt("Introduzca la palabra para adivinar.");
+		 palabra = palabra.split("");
+		 
+	 }while(palabra.length >= 10 || Number(palabra)){
+		 
+		 for(let i = 0; palabra,length; i++){
+			 
+			 mostrar.split("");
+			 
+		 }
+	 }
+	 
+ }
+ 
+ 
+ function mostrar(){
+	 
+	 const palabra = document.getElementById("intentos")
+	 
+	 
+	 
+ }
+ 
+ function introducirLetra(){
+	 
+	 
+	 
+	 
+	 
+	 
+ }
